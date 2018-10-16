3b9148e4ec5a73145066a13c101cfae9d7299a5a
Whaell <waellakour193@live.com>
2018-10-17 02:07:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,7 +44,7 @@ bot.on('message', function (user, userID, channelID, message, id, evt) {
                 let messageget = bot.getMessage(); 
                 messageget({channelID: channelID, limit: 50});
 
-                fs.appendFileSync('Archive.txt', Bugger.from(messageget), (err) => {
+                fs.appendFileSync('Archive.txt', Buffer.from(messageget), (err) => {
                     if (err) throw console.log('The file has not been saved');
                     console.log('The file has been saved!');
                     });
