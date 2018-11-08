52cb2dfc55a69464637b3636999acfdc030daa66
codenvy fun <Whaell@protonmail.com>
2018-11-08 20:26:53 +0000

    putting a bunch other json files, this doesn't really change anything in implementation

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ my garbage bot uwu
 
 
 I basically copied this from a tutorial to figure out how js works and now hopefully make something different along the way
+
+please dont be mean!!!
--- bot.js
+++ bot.js
@@ -1,15 +1,16 @@
+/*eslint-disable no-undef-expression */
 const fs = require('fs');
-var path = require('path');
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+
 // Configure logger settings
-logger.remove(logger.transports.Console); // Why are we doing this first?
+logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
-// Initialize Discord Bot
+// Initialize Discord Bot uwuw!!!
 var bot = new Discord.Client({
    token: auth.token,
    autorun: true
@@ -22,7 +23,7 @@ bot.on('ready', function (evt) {
 bot.on('message', function (user, userID, channelID, message, id, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
+    if (message.substring(0, 1) === '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
        
@@ -40,15 +41,19 @@ bot.on('message', function (user, userID, channelID, message, id, evt) {
                     to: channelID,
                     message: 'God I **LOVE** pee'
                 });
-            case 'archive': {
-                bot.getMessages({channelID: channelID, limit: 50});
-
-                fs.appendFileSync('Archive.txt', Buffer.from(getMessages), (err) => {
-                    if (err) throw console.log('The file has not been saved');
-                    console.log('The file has been saved!');
+            break;
+            case 'archive': {  // PLEASE clean this sometime PLEASE 
+                bot.getMessages({channelID: channelID, limit: 50}, function (err, messages){
+                    if (err) throw console.log('messages not received!')
+                    
+                    fs.appendFileSync('Archive.txt', Buffer.from(messages), function (err){
+                        if (err) throw console.log('The file has not been saved');
+                        console.log('The file has been saved!');
                     });
+
+                });
             }
-                }; 
+                }
             // Just add any case commands if you want to..
          }
      }
